956677f029d87049ec6ab1923982665cdffba27b
Marcelo Souza <marcelo.illumine@icloud.comuser.email marcelo.illumine@icloud.com>
2026-08-18 13:21:46 -0300

    chore: add capacitor for mobile deployment

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.threeminutes.forlife',
+  appName: '3 Minutes for Life',
+  webDir: 'dist'
+};
+
+export default config;
